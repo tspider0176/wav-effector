d81b008c2938c5701972b239494cfd93b6c69b2f
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-05-18 16:18:26 +0900

    Refactering effecter class accoding to rubocop

--- effector.rb
+++ effector.rb
@@ -9,58 +9,60 @@ require_relative 'effects/distortion_effect/distortion'
 require_relative 'effects/distortion_effect/overdrive'
 require_relative 'effects/distortion_effect/fuzz'
 
+# Treat effectring to wav files
 class WavEffector
   def initialize(file_name)
     @file_name = file_name
   end
 
-  def get_information
+  def information
     effect = Effect.new(@file_name)
     "#{effect.get_format}Peak:\t\t#{effect.get_peak}"
   end
 
   def normalize
     Normalization.new(@file_name).write
-    WavEffector.new("#{fileName}-normalized.wav")
+    WavEffector.new("#{file_name}-normalized.wav")
   end
 
   def distort
     Distortion.new(@file_name).write
-    WavEffector.new("#{fileName}-Distortion.wav")
+    WavEffector.new("#{file_name}-Distortion.wav")
   end
 
   def overdrive
     Overdrive.new(@file_name).write
-    WavEffector.new("#{fileName}-Overdrive.wav")
+    WavEffector.new("#{file_name}-Overdrive.wav")
   end
 
   def fuzz
     Fuzz.new(@file_name).write
-    WavEffector.new("#{fileName}-Fuzz.wav")
+    WavEffector.new("#{file_name}-Fuzz.wav")
   end
 
   def delay
     Delay.new(@file_name).write
-    WavEffector.new("#{fileName}-Delay.wav")
+    WavEffector.new("#{file_name}-Delay.wav")
   end
 
   def reverb
     Reverb.new(@file_name).write
-    WavEffector.new("#{fileName}-Reverb.wav")
+    WavEffector.new("#{file_name}-Reverb.wav")
   end
 
-private
-  def fileName
-    "#{@file_name.split('.').first}"
+  private
+
+  # Returns file name without flie extension
+  def file_name
+    @file_name.split('.').first
   end
 end
 
-effector = WavEffector.new("sample/piano.wav")
-puts effector.get_information
+effector = WavEffector.new('sample/piano.wav')
+puts effector.information
 # effector.distortion
 # effector.normalize.distortion
 # effector.overdrive
 # effector.normalize.overdrive
 # effector.fuzz
 # effector.normalize.fuzz
-effector.reverb
