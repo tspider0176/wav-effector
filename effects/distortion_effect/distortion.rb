fd8c6ada4a8176928e2ea828660813b306b3eb8f
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-05-18 16:01:06 +0900

    Apply rubocop to distortion class

--- effects/distortion_effect/distortion.rb
+++ effects/distortion_effect/distortion.rb
@@ -1,5 +1,6 @@
 require_relative './distortion_effect'
 
+# Implements distortion effect
 class Distortion < DistortionEffect
   def initialize(file_name)
     super(file_name)
@@ -10,14 +11,15 @@ class Distortion < DistortionEffect
     distort(peak)
   end
 
-private
+  private
+
   def sgn(x)
     x > 0 ? 1 : -1
   end
 
   def distort(peak)
-      @wavs.map{|data|
-        sgn(data) * (1.0 - Math.exp((-1.0) * data.abs)) * peak.to_f
-      }
+    @wavs.map do |data|
+      sgn(data) * (1.0 - Math.exp(-1.0 * data.abs)) * peak.to_f
+    end
   end
 end
