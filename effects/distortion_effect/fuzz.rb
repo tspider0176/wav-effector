1f9f6e5afac152d174225b5ad128d07f0f3a52cb
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-05-18 15:58:25 +0900

    Apply rubocop to Fuzz distortion class

--- effects/distortion_effect/fuzz.rb
+++ effects/distortion_effect/fuzz.rb
@@ -1,5 +1,6 @@
 require_relative './distortion_effect'
 
+# Implements Fuzz distortion
 class Fuzz < DistortionEffect
   def initialize(file_name)
     super(file_name)
@@ -10,10 +11,15 @@ class Fuzz < DistortionEffect
     fuzz(peak, 8, -0.2)
   end
 
-private
+  private
+
   def fuzz(peak, dist, q)
-    @wavs.map{|data|
-      data == (q * peak).to_i ? (((1.0/dist) + q*peak / 1 - Math.exp(dist * q*peak))).to_i : (((data - q*peak) / (1 - Math.exp((-1) * dist * (data - q*peak)))) + (q*peak / (1 - Math.exp(dist * q*peak)))).to_i
-    }
+    @wavs.map do |data|
+      if data == (q * peak).to_i
+        (((1.0 / dist) + q * peak / 1 - Math.exp(dist * q * peak))).to_i
+      else
+        (((data - q * peak) / (1 - Math.exp(-1 * dist * (data - q * peak)))) + (q * peak / (1 - Math.exp(dist * q * peak)))).to_i
+      end
+    end
   end
 end
